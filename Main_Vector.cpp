7228e4aad67bde62f372e4f0f1f93acb127e9414
Уханов Антон <tosha.uhanoff@yandex.ru>
2017-03-03 17:44:31 +0300

    Update Main_Vector.cpp

--- Main_Vector.cpp
+++ Main_Vector.cpp
@@ -1,4 +1,3 @@
-
 #include "stdafx.h"
 #include "iostream"
 #include "TVector.h"
@@ -10,6 +9,8 @@ int _tmain(int argc, _TCHAR* argv[])
 	vect1.Print();
 	TVector3 vect2(10,11,12);
 	vect2.Print();
+	TVector *vectdynamic1 = new TVector3(11, 153, 12); //Демонстрация динам. полиморф.
+	vectdynamic1->Print();
 	system("pause");
 	return 0;
 }
